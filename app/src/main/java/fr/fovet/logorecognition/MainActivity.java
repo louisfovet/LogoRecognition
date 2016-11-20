c1a683246c679eb8259841633792c739e7d13d63
Louis Fovet <louiscfovet@gmail.com>
2016-11-20 23:20:17 +0100

    Added TAKE+SELECT photo

--- app/src/main/java/fr/fovet/logorecognition/MainActivity.java
+++ app/src/main/java/fr/fovet/logorecognition/MainActivity.java
@@ -1,8 +1,16 @@
 package fr.fovet.logorecognition;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,9 +19,17 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.bytedeco.javacpp.opencv_highgui.imread;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_SELECT = 2;
 
     protected Button btnCapture;
     protected Button btnLibrary;
@@ -37,14 +53,14 @@ public class MainActivity extends AppCompatActivity {
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dispatchTakePictureIntent();
             }
         });
 
         btnLibrary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dispatchSelectPictureIntent();
             }
         });
 
@@ -54,6 +70,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && data != null) {
+
+            if(requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Uri takenPhoto = (Uri) extras.get(MediaStore.EXTRA_OUTPUT);
+                imgAnalysis.setImageURI(takenPhoto);
+            }
+
+            if(requestCode == REQUEST_IMAGE_SELECT) {
+                Uri selectedPhoto = data.getData();
+                imgAnalysis.setImageURI(selectedPhoto);
+            }
+
+        }
 
     }
 
@@ -77,5 +114,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                timeStamp,
+                ".jpg",
+                storageDir
+        );
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "fr.fovet.logorecognition",
+                        photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private void dispatchSelectPictureIntent() {
+        Intent selectPictureIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(selectPictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(selectPictureIntent, REQUEST_IMAGE_SELECT);
+        }
     }
 }
