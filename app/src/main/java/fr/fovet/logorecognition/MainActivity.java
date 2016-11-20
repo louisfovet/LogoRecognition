94af02e42c8d69171938fbf13546a637d8e054ab
Louis Fovet <louiscfovet@gmail.com>
2016-11-20 18:46:36 +0100

    Added OpenCV

--- app/src/main/java/fr/fovet/logorecognition/MainActivity.java
+++ app/src/main/java/fr/fovet/logorecognition/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static org.bytedeco.javacpp.opencv_highgui.imread;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,14 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
